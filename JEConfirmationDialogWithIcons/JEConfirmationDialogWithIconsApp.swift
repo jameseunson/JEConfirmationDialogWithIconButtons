47ff7ce8dff7d7537c7cee0db7f65f8df8376231
James Eunson <james.eunson@gmail.com>
2023-05-22 16:00:04 +1000

    Initial Commit

---
+++ JEConfirmationDialogWithIcons/JEConfirmationDialogWithIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JEConfirmationDialogWithIconsApp.swift
+//  JEConfirmationDialogWithIcons
+//
+//  Created by James Eunson on 22/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct JEConfirmationDialogWithIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
